f3a77897986d684c6e6618b330c8ed44ae79aeab
Gabriel Miranda <gabrielmfern@outlook.com>
2025-12-11 11:07:18 -0300

    chore: renovate (#72)
    * update nextjs
    
    * update turbo.json
    
    * remove unecessary dependencies, remove scripts filter, update turbo
    
    * remove npmrc
    
    * remove biome config
    
    * update react email dependencieds
    
    * use an api route instead of page
    
    * lint

---
+++ apps/web/app/api/send/route.tsx
@@ -0,0 +1,10 @@
+import { render } from '@react-email/components';
+import VercelInviteUserEmail from 'transactional/emails/vercel-invite-user';
+
+export async function GET() {
+  return new Response(await render(<VercelInviteUserEmail />), {
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  });
+}
